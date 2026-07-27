0c491567a82951073c086fb9fd880fe4b0d740c5
Leonardo Vianna <vianna@rocketmail.com>
2026-07-27 19:07:15 +0000

    Ajuste do modelo Openrouter

--- agent.py
+++ agent.py
@@ -75,7 +75,7 @@ api_server = {
     'openrouter' : {
         'base_url': 'https://openrouter.ai',
         'api_key': os.getenv('OPENROUTER_API_KEY'),
-        'modelo': 'nvidia/nemotron-3-ultra-550b-a55b:free'
+        'modelo': 'google/gemma-4-31b-it:free'
     }
 }
 
